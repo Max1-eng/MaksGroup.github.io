26bdc9dc1245170319862c29a881b3c7001c87a6
Time <65963413+Max1-eng@users.noreply.github.com>
2020-05-27 21:57:41 +0400

    Add files via upload

---
+++ Maks Group.js
@@ -0,0 +1,18 @@
+alert (' Maks Group saytina dahil oldunuz');
+let result = 0;
+
+for (let i = 1; i <= 3; i++) {
+	for (let j = 1; j <= 1; j++) {
+		console.log(1+''+i);
+	}
+}
+for (let i = 1; i <= 3; i++) {
+	for (let j = 1; j <= 2; j++) {
+		console.log(2+''+i);
+	}
+}
+for (let i = 1; i <= 3; i++) {
+	for (let j = 1; j <= 3; j++) {
+		console.log(3+''+i);
+	}
+}
