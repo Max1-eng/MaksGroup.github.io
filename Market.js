3f85c2889608563a155b82867ab7cf9b20b74c00
Time <65963413+Max1-eng@users.noreply.github.com>
2020-05-28 19:53:57 +0400

    Market.js

--- Market.js
+++ Market.js
@@ -9,15 +9,15 @@ var hours = d.getHours();
 var minutes = d.getMinutes();
 var seconds = d.getSeconds();
 
-month=new Array("th JANUARY", "th FEBRUARY", "th MARCH", "th APRIL", "th MAY", "th JUNE",
-"th JULY", "th AUGUST", "th SEPTEMBER", "th OKTOBER", "th NOVEMBER", "th DESEMREB");
+month=new Array("Yanvar", "Fevral", "Mart", "Aprel", "May", "İyun",
+"İyul", "Avgust", "Sentyabr", "Oktyabr", "Noyabr", "Dekabr");
 
 if (day <= 9) day = "0" + day;
 if (hours <= 9) hours = "0" + hours;
 if (minutes <= 9) minutes = "0" + minutes;
 if (seconds <= 9) seconds = "0" + seconds;
 
-date_time = "Today is-" + day + " " + month[month_num] + " " + d.getFullYear() +
+date_time = "Bu gün-" + day + " " + month[month_num] + " " + d.getFullYear() +
 " г.&nbsp;&nbsp;&nbsp;Hours-"+ hours + ":" + minutes + ":" + seconds;
 if (document.layers) {
  document.layers.doc_time.document.write(date_time);
